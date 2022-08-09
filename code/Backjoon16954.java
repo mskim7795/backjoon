903036871acd88c83c3c40bec60c77f8b9fdaaea
mskim7795 <62835628+mskim7795@users.noreply.github.com>
2022-08-09 22:31:53 +0900

    백준 16954 움직이는 미로 탈출 풀이

--- code/Backjoon16954.java
+++ code/Backjoon16954.java
@@ -6,8 +6,8 @@ import java.util.Queue;
 
 public class Backjoon16954 {
 
-    static char[][][] board = new char[9][8][8];
-    static boolean[][][] visited = new boolean[9][8][8];
+    static char[][][] board = new char[9][8][8]; // 움직일 때 마다 벽이 움직이므로 모든 벽이 사라지는 9번째 턴까지 체스판을 저장합니다.
+    static boolean[][][] visited = new boolean[9][8][8]; // 방문체크도 벽의 모양에 따라 9가지로 체크해줍니다.
     static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1, 0};
     static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1, 0};
 
@@ -34,6 +34,7 @@ public class Backjoon16954 {
             }
         }
 
+        // 체스판의 모양은 모든 벽이 사라지는 경우까지 총 9가지가 존재합니다. 9가지 체스판 모양을 생성합니다.
         for (int i = 1; i <= 8; i++) {
             for (int j = i - 1; j >= 0; j--) {
                 board[i][j] = new char[]{'.', '.', '.', '.', '.', '.', '.', '.'};
@@ -44,7 +45,9 @@ public class Backjoon16954 {
             }
         }
 
-        System.out.println(bfs());
+        bw.write(bfs());
+        bw.flush();
+        bw.close();
     }
 
     static int bfs() {
@@ -53,6 +56,8 @@ public class Backjoon16954 {
 
         while (!queue.isEmpty()) {
             Point point = queue.poll();
+
+            // 현재 벽 모양을 체크하기 위한 변수입니다.
             int count = Math.min(point.count, 8);
 
             if (point.x == 0 && point.y == 7) {
